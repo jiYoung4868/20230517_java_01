5eb1a08302eb8a6db2dadff8fdb3e697872b2af8
jiYoung4868 <orinogcode@gmail.com>
2023-06-13 12:06:45 +0900

    sender (미완성)

---
+++ 20230613_udp_sender/src/kh/lclass/udp/UdpEchoReceiver.java
@@ -0,0 +1,48 @@
+package kh.lclass.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class UdpEchoReceiver {
+	public static void main(String[] args) {
+		new UdpEchoReceiver().receiverUdp();
+	}
+
+	public void receiverUdp() {
+		int myPort = 6001;
+//		int destPort = 5001;
+		DatagramSocket dSock = null;
+		try {
+			dSock = new DatagramSocket(myPort);
+			while (true) {
+				byte[] byteMsg = new byte[1000];
+				DatagramPacket receivedData = new DatagramPacket(byteMsg, byteMsg.length);
+				dSock.receive(receivedData);
+				
+//				System.out.println(byteMsg.length);
+//				System.out.println(receivedData.getData().length);
+//				System.out.println(receivedData.getLength());
+//				System.out.println(receivedData.getAddress());
+//				System.out.println(receivedData.getPort());
+
+				String receivedStr = new String(receivedData.getData());
+				System.out.println("수신메시지: " + receivedStr);
+				
+				DatagramPacket sendData 
+				= new DatagramPacket
+				( receivedData.getData(), receivedData.getLength(), receivedData.getAddress(), receivedData.getPort());
+				
+				dSock.send(sendData); 
+			
+			}
+
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ 20230613_udp_sender/src/kh/lclass/udp/UdpEchoSender.java
@@ -0,0 +1,77 @@
+package kh.lclass.udp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.rmi.UnknownHostException;
+
+public class UdpEchoSender {
+	public static void main(String[] args) {
+		new UdpEchoSender().senderUdp();
+	
+
+	public void senderUdp() {
+		// 서버의 포트번호 정함
+		int myPort = 5001;
+		int destPort = 6001;
+		String destName = "localhost";
+		
+		DatagramSocket dSock = null;
+		BufferedReader br = null;
+		try {
+			// DatagramSocket 객체 생성
+			dSock = new DatagramSocket(myPort);
+			// 매개인자 없으면 자동port 번호 할당, 지정하면 해당 포트번호로 소켓 생성
+			
+			br = new BufferedReader(new InputStreamReader(system.in));
+
+			while (true) {
+
+				System.out.println("입력>>");
+				String sendMsg = br.readLine(); //콘솔 입력받음
+				//exit 입력하면 프로그램 끝내기
+				if(sendMsg.equals("exit")) {
+					break;
+				}
+				// 전달할 메시지
+				// 메시지 전달
+				InetAddress destIp = null;
+				try {
+					// 연결한 클라이언트 IP주소를 가진 InetAddress 객체 생성
+					destIp = InetAddress.getByName(destName);
+					// 전송할 메시지를 byte[]로 바꿈
+					byte[] byteMsg = sendMsg.getBytes();
+					// 전송할 메시지를 DatagramPacket 객체에 담음
+					DatagramPacket sendData = new DatagramPacket(byteMsg, byteMsg.length, destIp, destPort);
+
+					// 소켓 레퍼런스를 사용하여 메시지 전송
+					dSock.send(sendData);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+						e.printStackTrace();
+				}
+				//메시지 수신
+				byte[] byteMsg = new byte[1000];
+				DatagramPacket receivedData = new DatagramPacket(byteMsg, byteMsg.length);
+				try {
+					dSock.receive(receivedData);
+				
+				
+			
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		} finally {
+			if (dSock != null) dSock.close();
+		}
+	}
+		}
+}
